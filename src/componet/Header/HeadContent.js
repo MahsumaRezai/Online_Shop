b0ef1e3c60bb585070c1c5739a7a23e36537f16c
Masoumeh Rezai <you@example.com>
2023-08-02 04:02:02 +0430

    add Head content

---
+++ src/componet/Header/HeadContent.js
@@ -0,0 +1,14 @@
+import { Fragment } from "react";
+
+const HeadCotent = (props) => {
+    return (
+        <Fragment>
+            <ul>
+                
+            </ul>
+            
+
+        </Fragment>
+    )
+}
+export default HeadCotent;

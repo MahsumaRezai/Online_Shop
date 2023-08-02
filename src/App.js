6ca11532f4eed183cdd83c4ba1b4460cc84cde62
Masoumeh Rezai <you@example.com>
2023-08-02 05:20:09 +0430

    add link section

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import { Fragment } from "react";
 import Head from "./componet/Head/Head";
 import MainHead from "./componet/MainHead/MainHead";
 import Header from "./componet/Header/Header";
+import HeadCotent from "./componet/Header/HeadContent";
 
 
 function App() {
   return (
     <Fragment>
-      <Head/>
-      <Header/>
-      <MainHead/>
+      <Head />
+      <HeadCotent />
+     
+
+      <Header />
+      <MainHead />
     </Fragment>
 
   );
--- src/componet/Header/HeadContent.js
+++ src/componet/Header/HeadContent.js
@@ -1,12 +1,14 @@
 import { Fragment } from "react";
+import { Link } from 'react-router-dom'
 
 const HeadCotent = (props) => {
     return (
         <Fragment>
             <ul>
-                
+                <li><Link to='/home'>Home</Link></li>
+
             </ul>
-            
+
 
         </Fragment>
     )

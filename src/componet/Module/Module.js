a5602f4d00b4ea2d8feaf4ba005c044e36530dec
Masoumeh Rezai <you@example.com>
2023-08-03 08:51:01 +0430

    add card

---
+++ src/componet/Module/Module.js
@@ -0,0 +1,21 @@
+import Card from './Card'
+const Module = (props) => {
+    return (
+        <Card>
+            <form>
+                <label htmlFor='Type of Glasses'>Type of Glasses</label>
+                <input />
+                <label htmlFor='Add Price'>Add Price</label>
+                <input />
+                <button>Send</button>
+
+
+
+
+            </form>
+
+        </Card>
+
+    )
+}
+export default Module

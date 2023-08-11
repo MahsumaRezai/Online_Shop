e8d0b9e68e7733448c0021448ccc361c7bd0ef92
Masoumeh Rezai <you@example.com>
2023-08-11 08:02:14 +0430

    add form for contact section

--- src/componet/Contact/Contac.js
+++ src/componet/Contact/Contac.js
@@ -1,10 +1,11 @@
 import { Fragment } from "react";
 import HeadProduct from "../Product/HeadProduct";
-
+import Headers from "../Product/Headers";
 const Contac = (props) => {
     return (
         <Fragment>
             <HeadProduct />
+            <Headers />
 
         </Fragment>
     )
---
+++ src/componet/Contact/Form.js
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+
+const Form = (props) => {
+    return (
+        <Fragment>
+            <form>
+                <label htmlFor="name"></label>
+                <input id="name" />
+            </form>
+        </Fragment>
+    )
+}
